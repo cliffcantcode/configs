b5cad4ddc79e20f74b2770dea119a2e793babc25
cliffcantcode <cliffcantcode@gmail.com>
2022-11-19 17:47:07 -0500

    added more vim.opt s

--- lua/cliffcantcode/options.lua
+++ lua/cliffcantcode/options.lua
@@ -1,21 +1,36 @@
-vim.opt.backup = false				            -- creates a backup file
-vim.opt.clipboard = "unnamedplus"		      -- allows neovim access to the system clipboard
-vim.opt.cmdheight = 1				              -- more space in the neovim command line for displaying messages
-vim.opt.hlsearch = true                   -- highlight all matches on previous search pattern
-vim.opt.fileencoding = "utf-8"            -- the encoding written to a file
-vim.opt.mouse = "a"				                -- allow the mouse to be used in neovim
-vim.opt.showtabline = 0				            -- always show tabs
-vim.opt.expandtab = true			            -- change tabs to spaces
-vim.opt.shiftwidth = 2                    -- the number of spaces to add per indentation
-vim.opt.tabstop = 2                       -- insert 2 spaces for a tab
-vim.opt.smartindent = true 			          -- make indenting smarter
-vim.opt.splitright = true                 -- force all screen splits to be to the right of the current window 
-vim.opt.splitbelow = true                 -- force all screen splits to be below the current window
-vim.opt.cursorline = false			          -- highlight the current line; TODO change once colors are better
-vim.opt.number = true				              -- set numbered lines
-vim.opt.relativenumber = true			        -- sets line numbers to be increasing away from the cursor's line
-vim.opt.ruler = false                     -- hide the line and column position of the cursor
-vim.opt.wrap = false				              -- display lines as one long line
-vim.opt.scrolloff = 8				              -- minimal number of lines to keep above or below the cursor
-vim.opt.iskeyword:append("-")			        -- treat words with '-' as single words
-vim.opt.whichwrap:append("<,>,[,],h,l")   -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.backup = false				                  -- creates a backup file
+vim.opt.clipboard = "unnamedplus"		            -- allows neovim access to the system clipboard
+vim.opt.cmdheight = 1				                    -- more space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect"}  -- mostly for cmp
+vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true                       -- ignore case in search patterns
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.mouse = "a"				                      -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10                          -- pop up menu height
+vim.opt.showtabline = 0				                  -- always show tabs
+vim.opt.showmode = false                        -- don't need to see things like -- INSERT -- anymore
+vim.opt.expandtab = true			                  -- change tabs to spaces
+vim.opt.shiftwidth = 2                          -- the number of spaces to add per indentation
+vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.numberwidth = 4                         -- minimal number of columns to use for the line number {default 4}
+vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time 
+vim.opt.smartcase = true                        -- smart case
+vim.opt.smartindent = true 			                -- make indenting smarter
+vim.opt.splitright = true                       -- force all screen splits to be to the right of the current window 
+vim.opt.splitbelow = true                       -- force all screen splits to be below the current window
+vim.opt.swapfile = false                        -- creates a swapfile
+vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (milliseconds)
+vim.opt.undofile = true                         -- enables persistent undo
+vim.opt.updatetime = 300                        -- faster completion (4000 ms default)
+vim.opt.cursorline = false 			                -- highlight the current line; TODO change once colors are better
+vim.opt.number = true				                    -- set numbered lines
+vim.opt.relativenumber = true			              -- sets line numbers to be increasing away from the cursor's line
+vim.opt.showcmd = false                         -- hide (partial) command in the last line of the screen (for performance)
+vim.opt.ruler = false                           -- hide the line and column position of the cursor
+vim.opt.wrap = false				                    -- display lines as one long line
+vim.opt.scrolloff = 8				                    -- minimal number of lines to keep above or below the cursor
+vim.opt.iskeyword:append("-")			              -- treat words with '-' as single words
+vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.linebreak = true
