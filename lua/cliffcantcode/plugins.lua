740063edb2bf316e2145f279f29314e106832f16
cliffcantcode <cliffcantcode@gmail.com>
2022-11-22 15:07:25 -0500

    added dap

--- lua/cliffcantcode/plugins.lua
+++ lua/cliffcantcode/plugins.lua
@@ -89,6 +89,11 @@ return packer.startup(function(use)
 	  commit = "8e763332b7bf7b3a426fd8707b7f5aa85823a5ac",
 	}
 
+  -- DAP
+  use { "mfussenegger/nvim-dap", commit = "6b12294a57001d994022df8acbe2ef7327d30587" }
+  use { "rcarriga/nvim-dap-ui", commit = "1cd4764221c91686dcf4d6b62d7a7b2d112e0b13" }
+  use { "ravenxrz/DAPInstall.nvim", commit = "8798b4c36d33723e7bba6ed6e2c202f84bb300de" }
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
